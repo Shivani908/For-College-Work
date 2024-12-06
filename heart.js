03d727f274c7a30ee2e9173b0507005bbe5da261
Shivani90 <jyotishukla05274@gmail.com>
2024-12-06 23:05:29 +0530

    bome

--- heart.js
+++ heart.js
@@ -2,6 +2,14 @@ let n = 10;
 let str = "";
 for (let i = n/2; i<n; i += 2){
     for(let j=1; j<n -i; j+=2){
-        str += "";
+        str += " ";
     }
+    for(let j=1; j<i +1; j++){
+        str += "*";
+    }
+    for(let j=1; j<n-i +1; j++){
+        str += " ";
+    }
+
+    
 }
