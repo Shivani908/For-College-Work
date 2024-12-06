ad44667371cb0bad65b03dd148a8bd02a259b782
Shivani90 <jyotishukla05274@gmail.com>
2024-12-06 23:10:06 +0530

    bnbnbn

--- heart.js
+++ heart.js
@@ -15,3 +15,13 @@ for (let i = n/2; i<n; i += 2){
     }
     str += "\n"
 }
+
+for(let i=n; i>0; i--){
+    for(let j=0; j<n -i; j++){
+        str += " ";
+    }
+    for(let j=1; j<i*2; j++){
+        str += "*";
+    }
+    
+}
